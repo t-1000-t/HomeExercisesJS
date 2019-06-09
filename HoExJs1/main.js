258ced8b470e4f7545a3c91d1cf7719dc7be8094
vlad <goognya@gmail.com>
2019-06-09 11:01:33 +0300

    home 1 ready

---
+++ HoExJs1/main.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-alert */
+// eslint-disable-next-line no-unused-vars
+const ADMIN_PASSWORD = 'm4ng0h4ckz';
+// eslint-disable-next-line no-unused-vars
+let messege;
+
+// eslint-disable-next-line no-unused-vars
+const PASS_QUESTION = prompt('Enter the password');
+if (PASS_QUESTION === null) {
+  messege = 'Отменено пользователем!';
+  alert(messege);
+}
+if (PASS_QUESTION === ADMIN_PASSWORD) {
+  messege = 'Добро пожаловать!';
+  alert(messege);
+} else {
+  messege = 'Доступ запрещен, неверный пароль!';
+  alert(messege);
+}
