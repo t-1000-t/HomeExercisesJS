e7df96f588ed29c9e565cd813a3626460fadd2a7
vlad <goognya@gmail.com>
2019-06-18 20:03:40 +0300

    Module 2 home 1 correct

--- Module2/HoExJs1/main.js
+++ Module2/HoExJs1/main.js
@@ -1,26 +1,25 @@
-/* eslint-disable no-alert */
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
 let input;
 const numbers = [];
-// eslint-disable-next-line prefer-const
 let total = 0;
 
 do {
   // eslint-disable-next-line no-shadow
   let input = prompt('write numbers:', '');
+
   if (input === null) break;
+
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(input)) {
+    alert('Было введено не число, попробуйте еще раз');
+    // eslint-disable-next-line no-continue
+    continue;
+  }
   input = Number(input);
   numbers.push(input);
-// eslint-disable-next-line no-constant-condition
 } while (true);
+
 for (let i = 0; i < numbers.length; i += 1) {
-  // eslint-disable-next-line no-restricted-globals
-  if (isNaN(numbers[i])) {
-    // eslint-disable-next-line no-alert
-    alert('Было введено не число, попробуйте еще раз');
-  } else {
-    total += numbers[i];
-  }
+  total += numbers[i];
 }
-// eslint-disable-next-line no-alert
 alert(total);
